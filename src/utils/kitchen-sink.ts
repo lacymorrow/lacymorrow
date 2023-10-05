fbcc273d43bec66e41ea37839d5bf6e7b18fcce9
Lacy Morrow <me@lacymorrow.com>
2023-10-05 19:33:08 -0400

    fix build

--- src/utils/kitchen-sink.ts
+++
@@ -1,9 +0,0 @@
-// <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4" >
-// 	<code className="text-white" > { JSON.stringify(data, null, 2) } </code>
-// </pre>
-
-
-// const primaryFont = Inter({
-// 	subsets: ["latin"],
-// 	variable: "--primary-font",
-// });
