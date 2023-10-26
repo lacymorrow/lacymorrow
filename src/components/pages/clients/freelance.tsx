86300b2701aa019ba79d3162371dbff95f29bd61
Lacy Morrow <me@lacymorrow.com>
2023-10-25 21:46:50 -0400

    mg

--- src/components/pages/clients/freelance.tsx
+++ src/components/pages/clients/freelance.tsx
@@ -1,10 +1,74 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Masonry from "react-masonry-component";
+// import { Masonry } from "masonic";
+
+const items = [
+  {
+    name: "American Comfort Solutions",
+    src: "/static/clients/th-american.jpg",
+  },
+  {
+    name: "College Career Advisors",
+    src: "/static/clients/th-cca.jpg",
+  },
+  {
+    name: "Charlie's Vision",
+    src: "/static/clients/th-charliesvision.jpg",
+  },
+  {
+    name: "Cosmic Cart",
+    src: "/static/clients/th-cosmiccart.jpg",
+  },
+  {
+    name: "Effective",
+    src: "/static/clients/th-effective.jpg",
+  },
+  {
+    name: "Hinson",
+    src: "/static/clients/th-hinson.jpg",
+  },
+  {
+    name: "IAC",
+    src: "/static/clients/th-iac.jpg",
+  },
+  {
+    name: "Innovation",
+    src: "/static/clients/th-innovation.jpg",
+  },
+  {
+    name: "Invision",
+    src: "/static/clients/th-invision.jpg",
+  },
+  {
+    name: "Kim Brattain",
+    src: "/static/clients/th-kimbrattain.jpg",
+  },
+  {
+    name: "Mission Film",
+    src: "/static/clients/th-missionfilm.jpg",
+  },
+];
+
+interface MasonryCardProps {
+  index: number;
+  data: {
+    name: string;
+    src: string;
+  };
+  width: number;
+}
+
+const MasonryCard = ({
+  index,
+  data: { src, name },
+  width,
+}: MasonryCardProps) => <img src={src} alt={name} />;
 
 const Freelance = () => {
   return (
     <div className="my-8">
+      {/* <Masonry items={items} render={MasonryCard} /> */}
       {/* @ts-ignore */}
       <Masonry enableResizableChildren={true}>
         <div>
