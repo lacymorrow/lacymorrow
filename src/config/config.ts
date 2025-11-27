9ad9927cf0253c27b468a3abe1c687450c2f11fc
Lacy Morrow <me@lacymorrow.com>
2025-11-27 17:37:48 -0500

    auto commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const receivingEmail = process?.env?.RECEIVING_EMAIL || 'lacymorrow0@gmail.com';
 
-export const sendgridApiKey = process.env.SENDGRID_API_KEY;
+export const resendApiKey = process.env.RESEND_API_KEY;
