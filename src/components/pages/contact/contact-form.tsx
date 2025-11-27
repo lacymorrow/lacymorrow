9f57b5e03a74d5b3bd910a80caf2612b9ebc01d3
Lacy Morrow <me@lacymorrow.com>
2025-11-27 17:41:51 -0500

    auto commit

--- src/components/pages/contact/contact-form.tsx
+++ src/components/pages/contact/contact-form.tsx
@@ -188,11 +188,8 @@ const ContactForm = () => {
             </div>
 
             <div className="grid gap-2">
-              <Label htmlFor="name">
-                Name<sup>*</sup>
-              </Label>
+              <Label htmlFor="name">Name</Label>
               <Input
-                required
                 id="name"
                 type="name"
                 name="name"
