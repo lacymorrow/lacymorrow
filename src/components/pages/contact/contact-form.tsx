90976689ba96a4c77f53695a299a2b62c238a1f3
Lacy Morrow <me@lacymorrow.com>
2025-11-27 17:41:05 -0500

    auto commit

--- src/components/pages/contact/contact-form.tsx
+++ src/components/pages/contact/contact-form.tsx
@@ -24,7 +24,6 @@ interface StateType {
   name: string;
   email: string;
   tel: string;
-  subject: string;
   message: string;
 }
 
@@ -32,7 +31,6 @@ const defaults: StateType = {
   name: "",
   email: "",
   tel: "",
-  subject: "",
   message: "",
 };
 
@@ -205,16 +203,6 @@ const ContactForm = () => {
               />
             </div>
 
-            <div className="grid gap-2">
-              <Label htmlFor="subject">Subject</Label>
-              <Input
-                id="subject"
-                name="subject"
-                placeholder="I need help with..."
-                onChange={handleChange}
-                value={state.subject}
-              />
-            </div>
             <div className="grid gap-2">
               <Label htmlFor="message">
                 Message<sup>*</sup>
