48b6ac914096fececa07f63ffe15cb82c60e858c
Lacy Morrow <me@lacymorrow.com>
2026-03-16 12:16:17 -0400

    fix: add all client pages to nav, exclude hidden pages from sitemap
    - Added all 20 client pages to work/clients/_meta.json (was only 3)
    - Excluded /sink, /lacy, /404 from sitemap (intentionally hidden)
    - Fixes Ahrefs orphan pages warning

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.lacymorrow.com',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
+  exclude: ['/sink', '/lacy', '/404'],
 }
